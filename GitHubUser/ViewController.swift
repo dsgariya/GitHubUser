1261e1e00d34a7a097b07a20bed026e727d33359
Singh, Dev | Dev | LPD <dev.singh@rakuten.com>
2025-06-24 14:53:35 +0900

    Initial Commit

---
+++ GitHubUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubUser
+//
+//  Created by Singh, Dev | Dev | LPD on 2025/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHubUserUITests/GitHubUserUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  GitHubUserUITestsLaunchTests.swift
+//  GitHubUserUITests
+//
+//  Created by Singh, Dev | Dev | LPD on 2025/06/24.
+//
+
+import XCTest
+
+final class GitHubUserUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
